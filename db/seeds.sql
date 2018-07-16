f463377819a900327d53aa4e4462fac653392354
Gustavo Canales <gustavo.canales@me.com>
2018-07-16 17:41:35 -0500

    Get and post working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (name) VALUES ("Cheeseburger");
+INSERT INTO burger (name) VALUES ("Baconburger");
+INSERT INTO burger (name) VALUES ("Chicker Burger");
---
+++ public/assets/js/burger.js
@@ -0,0 +1,26 @@
+$(function () {
+
+    $(".addBurger").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+    
+        var newBurger = {
+          burger_name: $("#burgerToeat").val().trim(),
+        };
+        //console.log(newBurger.name)
+    
+    
+        // Send the POST request.
+        $.ajax("/api/burger", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function () {
+            console.log("created new burger");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+
+});
